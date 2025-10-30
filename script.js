6f0cac3af750eb137eab50c8c581867a64bb6781
tahir <haron@gmail.com>
2025-10-29 18:44:22 -0700

    time changed and number added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('rsvpButton').addEventListener('click', function() {
   const rsvpDiv = document.getElementById('rsvpMessage');
-  rsvpDiv.textContent = "RSVP to MOMMY";
+  rsvpDiv.textContent = "RSVP to MOMMY  0722702432";
 });
